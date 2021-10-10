31d7482131ecd5532685a8b97c7657cda31d1faf
Rachit Chaudhary <rachit.chaudhary.17@mountblue.tech>
2021-10-10 21:08:19 +0530

    Setup App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React, { Component } from "react";
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
+class App extends Component {
+    render() {
+        return(
+            <React.Fragment>
+                <h3>Hello From App</h3>
+            </React.Fragment>
+        )
+    }
+}
 
 export default App;

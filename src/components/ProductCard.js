451da0a05a55c2c9c43b80b1fa302babf68113d9
pyarelal-sahu <pyarela.sahu.17@mountblue.tech>
2021-10-15 17:24:41 +0530

    ProductCard component added

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ProductCard(props) {
-    return (
-        <div className="product-card">
-            <Link to={`/productDetails/${props.productDetails.id}`}>
-                <div className="card-image-wrapper">
-                    <img src={props.productDetails.image} className="product-card-image" alt="productImage" />
-                </div>
-                <div className="card-body">
-                    <h3 className="product-price">&#8377; {props.productDetails.price}</h3>
-                    <span className="badge bg-warning text-dark">{props.productDetails.rating.rate} <i className="fas fa-star" /></span>
-                    <h3 className="product-title">{props.productDetails.title}</h3>
-                </div>
-            </Link>
-        </div>
-    )
+	return (
+		<div className="product-card">
+			<Link to={`/productDetails/${props.productDetails.id}`}>
+				<div className="card-image-wrapper">
+					<img
+						src={props.productDetails.image}
+						className="product-card-image"
+						alt="productImage"
+					/>
+				</div>
+				<div className="card-body">
+					<h3 className="product-price">
+						&#8377; {props.productDetails.price}
+					</h3>
+					<div className="rating">
+						{props.productDetails.rating.rate}{" "}
+						<i className="fas fa-star" />
+					</div>
+				</div>
+				<h3 className="product-title">{props.productDetails.title}</h3>
+			</Link>
+		</div>
+	);
 }

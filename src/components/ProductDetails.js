b9dacb8f54521dcec4f8460f7f4d0fb8c0b03549
pyarelal-sahu <pyarela.sahu.17@mountblue.tech>
2021-10-15 17:00:07 +0530

    Add ProductDetails component

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,15 +1,46 @@
 import React, { Component } from "react";
-// import { Link } from "react-router-dom";
-// import { Switch, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../App.css";
 
-
 export default class productDetails extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			productDetails: {},
+		};
+	}
 
+	componentDidMount() {
+		fetch(`https://fakestoreapi.com/products/${this.props.match.params.id}`)
+			.then((res) => res.json())
+			.then((result) => {
+				this.setState({
+					productDetails: result,
+				});
+				console.log(this.state.productDetails);
+			});
+	}
 	render() {
 		return (
 			<div className="product-details-page">
 				<h1>Product Details</h1>
+				<div>
+					<div>
+						<img
+							src={this.state.productDetails.image}
+							alt={this.state.productDetails.title}
+						/>
+					</div>
+					<div>
+						<h3>{this.state.productDetails.title}</h3>
+						<p> {this.state.productDetails.description} </p>
+						<button type="submit"> Add to cart </button>
+						<Link to="/">
+							{" "}
+							<button type="submit"> Back </button>{" "}
+						</Link>
+					</div>
+				</div>
 			</div>
 		);
 	}

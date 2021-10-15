a9c8d7bcdc300f16a1b030d520b6419a0969cff7
pyarelal-sahu <pyarelal.sahu.17@mountblue.tech>
2021-10-15 16:27:12 +0530

    /account added to Link tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,33 +5,34 @@ import "../App.css";
 import MiniNavbar from "./MiniNavbar";
 
 export default class Header extends Component {
-  render() {
-    return (
-      <section className="header-section">
-        <Logo />
-        <nav className="navbar">
-          <Link to="/" className="header-nav-link">
-            <i className="fas fa-house-user"></i>Home
-          </Link>
-          <Link to="/" className="header-nav-link">
-            <i className="far fa-user"></i>Account
-          </Link>
-          <Link to="/contact" className="header-nav-link">
-            <i className="fas fa-mobile-alt"></i>Contact
-          </Link>
-        </nav>
-        <div className="header-buttons-section">
-          <Link to="/cart" className="button-cart">
-            <i className="fas fa-shopping-cart"></i> My Cart
-          </Link>
-          <Link to="/signup">
-            <button className="button-login-register">
-              <i className="fas fa-sign-in-alt"></i> Login/Register
-            </button>
-          </Link>
-        </div>
-        <MiniNavbar />
-      </section>
-    );
-  }
+	render() {
+		return (
+			<section className="header-section">
+				<Logo />
+				<nav className="navbar">
+					<Link to="/" className="header-nav-link">
+						<i className="fas fa-house-user"></i>Home
+					</Link>
+					<Link to="/account" className="header-nav-link">
+						<i className="far fa-user"></i>Account
+					</Link>
+					<Link to="/contact" className="header-nav-link">
+						<i className="fas fa-mobile-alt"></i>Contact
+					</Link>
+				</nav>
+				<div className="header-buttons-section">
+					<Link to="/cart" className="button-cart">
+						<i className="fas fa-shopping-cart"></i> My Cart
+					</Link>
+					<Link to="/signup">
+						<button className="button-login-register">
+							<i className="fas fa-sign-in-alt"></i>{" "}
+							Login/Register
+						</button>
+					</Link>
+				</div>
+				<MiniNavbar />
+			</section>
+		);
+	}
 }
